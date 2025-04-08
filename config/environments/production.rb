6918ab68490c241080c7d4cacbdb9096114ae0f0
punitcse <jainpunit786@gmail.com>
2025-04-08 18:16:20 +0100

    add hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
+  config.hosts << "project-management-4vnm.onrender.com"
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
